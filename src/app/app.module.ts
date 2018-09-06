30e8cfaf5a15cc4ab313716e141240f5705a5df4
Pradip Mondal <pradip@Sudiptas-MacBook-Air.local>
2018-09-06 17:50:27 +0530

    initial page created

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,6 +6,7 @@ import { StatusBar } from '@ionic-native/status-bar';
 
 import { MyApp } from './app.component';
 import { HomePage } from '../pages/home/home';
+import { InitialPageModule } from '../pages/initial/initial.module';
 
 @NgModule({
   declarations: [
@@ -14,7 +15,8 @@ import { HomePage } from '../pages/home/home';
   ],
   imports: [
     BrowserModule,
-    IonicModule.forRoot(MyApp)
+    IonicModule.forRoot(MyApp),
+    InitialPageModule
   ],
   bootstrap: [IonicApp],
   entryComponents: [
